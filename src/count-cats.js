075de5658286c688539cc9a3553cfe9398585aed
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-09-09 17:09:13 +0200

    carbon-dating, count-cats, dream-team tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  return backyard.reduce((total, item) => {
+    return total + item.reduce((total2, item2) => {
+      return item2 === '^^' ? total2 += 1 : total2;
+    }, 0);
+  }, 0);
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) === false) return false;
+  const secretName = members.reduce((res, member) => {
+    return (typeof member === 'string') ? [...res, member.trim()[0].toUpperCase()] : res;
+  }, []);
+  return secretName.sort().join('');
 };
